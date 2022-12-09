523102f41eda4254bdce68bcd75c8731553e973d
unknown <gabers20000@gmail.com>
2022-12-09 18:15:14 -0300

    feat: dependecias do back-end, diagrama e inicio do react

---
+++ back/src/routes/users.routes.ts
@@ -0,0 +1,17 @@
+
+import { Router } from "express";
+// import deleteUserController from "../controllers/deleteUser.controller";
+// import listUserController from "../controllers/listUser.controller";
+// import listUsersController from "../controllers/listUsers.controller";
+// import updateUserController from "../controllers/updateUser.controller";
+// import userCreateController from "../controllers/userCreate.controller";
+
+const routes = Router();
+
+// routes.post("/", userCreateController);
+// routes.get("/", listUsersController);
+// routes.get("/:id", listUserController);
+// routes.delete("/:id", deleteUserController);
+// routes.patch("/:id", updateUserController);
+
+export default routes;
